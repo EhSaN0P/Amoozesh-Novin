10278441b1f6a4696de54a56fec429f5a6667fe4
EhSaN <ehsanog13@gmail.com>
2025-11-25 16:06:48 +0330

    single teacher added

---
+++ public/js/single_teacher.js
@@ -0,0 +1,15 @@
+new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 4,  // تعداد اسلایدهای نمایش داده شده روی دسکتاپ
+    spaceBetween: 30,  // فاصله بین اسلایدها
+    navigation: {
+        nextEl: '.sbn1',
+        prevEl: '.sbp1',
+    },
+    breakpoints: {
+        1920: { slidesPerView: 4, spaceBetween: 40 }, // دسکتاپ بزرگ
+        1280: { slidesPerView: 3, spaceBetween: 35 }, // لپ‌تاپ
+        768: { slidesPerView: 2, spaceBetween: 25 },  // تبلت
+        480: { slidesPerView: 1.2, spaceBetween: 20 } // موبایل
+    }
+});
