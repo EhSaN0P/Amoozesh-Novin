be65e16bcb61250ce894105b0037ae574d0214f1
EhSaN <ehsanog13@gmail.com>
2025-11-26 12:32:53 +0330

    style updated for btns and see more button added

--- public/js/landing_page.js
+++ public/js/landing_page.js
@@ -39,10 +39,7 @@ new Swiper('.swiper-container3', {
     loop: true,
     slidesPerView: 4,  // تعداد اسلایدهای نمایش داده شده روی دسکتاپ
     spaceBetween: 30,  // فاصله بین اسلایدها
-    navigation: {
-        nextEl: '.sbn3',
-        prevEl: '.sbp3',
-    },
+  
     breakpoints: {
         1920: { slidesPerView: 4, spaceBetween: 40 }, // دسکتاپ بزرگ
         1280: { slidesPerView: 3, spaceBetween: 35 }, // لپ‌تاپ
