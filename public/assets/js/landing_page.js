5909144de6e26ffac2c3874d3f36dd41cb58e097
EhSaN <ehsanog13@gmail.com>
2025-12-03 20:03:01 +0330

    font added and carousel centered

--- public/assets/js/landing_page.js
+++ public/assets/js/landing_page.js
@@ -1,48 +1,83 @@
-singleSliderTouchable("#bootstrapCarousel")
+new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 40,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    slidesPerView: 1, // موبایل
+
+    breakpoints: {
+        768: {            // تبلت
+            spaceBetween: 20,
+        },
+        1280: {           // لپ‌تاپ
+            spaceBetween: 30,
+        },
+        1920: {           // دسکتاپ بزرگ
+            spaceBetween: 40,
+        }
+    }
+});
+
+
+
+
 // Slider 1
 new Swiper('.swiper-container', {
     loop: true,
-    slidesPerView: 4,  // تعداد اسلایدهای نمایش داده شده روی دسکتاپ
-    spaceBetween: 30,  // فاصله بین اسلایدها
+    spaceBetween: 20,
     navigation: {
         nextEl: '.sbn1',
         prevEl: '.sbp1',
     },
+
     breakpoints: {
-        1920: { slidesPerView: 4, spaceBetween: 40 }, // دسکتاپ بزرگ
-        1280: { slidesPerView: 3, spaceBetween: 35 }, // لپ‌تاپ
-        768: { slidesPerView: 2, spaceBetween: 25 },  // تبلت
-        480: { slidesPerView: 1.2, spaceBetween: 20 }, // موبایل
-        100:  { slidesPerView: 1, spaceBetween: 10, centeredSlides: false }
-
-        
+        480: {       // موبایل بزرگ
+            slidesPerView: 1,
+            spaceBetween: 16,
+        },
+        768: {       // تبلت
+            slidesPerView: 2,
+            spaceBetween: 25,
+        },
+        1280: {      // لپ‌تاپ
+            slidesPerView: 3,
+            spaceBetween: 35,
+        },
+        1920: {      // دسکتاپ بزرگ
+            slidesPerView: 4,
+            spaceBetween: 40,
+        }
     }
-
 });
 
 
 
 
-
-new Swiper('.swiper-container2 ', {
+new Swiper('.swiper-container2', {
     loop: true,
-    slidesPerView: 4,  // تعداد اسلایدهای نمایش داده شده روی دسکتاپ
-    spaceBetween: 30,  // فاصله بین اسلایدها
+    slidesPerView: 1,  // موبایل
+    spaceBetween: 15,
+
     navigation: {
         nextEl: '.sbn2',
         prevEl: '.sbp2',
     },
-    breakpoints: {
-        1920: { slidesPerView: 4, spaceBetween: 40 }, // دسکتاپ بزرگ
-        1280: { slidesPerView: 3, spaceBetween: 35 }, // لپ‌تاپ
-        768: { slidesPerView: 2, spaceBetween: 25 },  // تبلت
-        480: { slidesPerView: 1.2, spaceBetween: 20 },
-        100:  { slidesPerView: 1.2, spaceBetween: 11, }
 
-        
-        // موبایل
+    breakpoints: {
+        480: { slidesPerView: 1.15, spaceBetween: 18 }, // موبایل بزرگ
+        768: { slidesPerView: 2, spaceBetween: 25 },    // تبلت
+        1280: { slidesPerView: 3, spaceBetween: 35 },   // لپ‌تاپ
+        1920: { slidesPerView: 4, spaceBetween: 40 },   // دسکتاپ بزرگ
     }
 });
+
 
 
 
@@ -54,15 +89,15 @@ new Swiper('.swiper-container3', {
     loop: true,
     centeredSlides: true,
     centeredSlidesBounds: true,
-    spaceBetween: 35,
+    spaceBetween: 32,
     initialSlide: 0,
     loopedSlides: 6,
     breakpoints: {
         1920: { slidesPerView: 3, loopedSlides: 3 },
-        1280: { slidesPerView: 3, loopedSlides: 3 },
+        1280: { slidesPerView: 2.5, loopedSlides: 2.5 },
         768:  { slidesPerView: 2, loopedSlides: 2 },
-        480:  { slidesPerView: 1, loopedSlides: 1 },
-        100:  { slidesPerView: 1, loopedSlides: 0.7, }
+        480:  { slidesPerView: 1.2, loopedSlides: 1.2 },
+        100:  { slidesPerView: 1, loopedSlides: 1, }
 
     }
 });
