2d7faf30f416a8ac001411375cc1af4d83ed38f3
EhSaN <ehsanog13@gmail.com>
2025-12-04 17:43:21 +0330

    new edits

--- public/assets/js/single_major.js
+++ public/assets/js/single_major.js
@@ -1,48 +1,55 @@
+
+// Slider 1
 new Swiper('.swiper-container', {
     loop: true,
-    slidesPerView: 4,  // تعداد اسلایدهای نمایش داده شده روی دسکتاپ
-    spaceBetween: 30,  // فاصله بین اسلایدها
+    spaceBetween: 20,
     navigation: {
         nextEl: '.sbn1',
         prevEl: '.sbp1',
     },
-    breakpoints: {
-        1920: { slidesPerView: 4, spaceBetween: 40 }, // دسکتاپ بزرگ
-        1280: { slidesPerView: 3, spaceBetween: 35 }, // لپ‌تاپ
-        768: { slidesPerView: 2, spaceBetween: 25 },  // تبلت
-        480: { slidesPerView: 1.2, spaceBetween: 20 }, // موبایل
-        100:  { slidesPerView: 1, spaceBetween: 10, centeredSlides: false }
-
-        
-    }
-
-});
-
-  
-new Swiper('.swiper-container3', {
-    loop: true,
-    initialSlide: 2,
-    spaceBetween: 35,
-    centeredSlides: true,   // دسکتاپ
 
     breakpoints: {
-        1920: { slidesPerView: 3 },
-        1280: { slidesPerView: 3 },
-        768:  { slidesPerView: 2 },
-
-        480:  { 
+        480: {       // موبایل بزرگ
             slidesPerView: 1,
-            centeredSlides: true,   // موبایل هم باید روشن باشه
-            initialSlide: 0         // موبایل همیشه باید ۰ باشه
+            spaceBetween: 16,
         },
-
-        100:  { 
-            slidesPerView: 0.7,
-            centeredSlides: true,
-            initialSlide: 0
+        768: {       // تبلت
+            slidesPerView: 2,
+            spaceBetween: 25,
+        },
+        1280: {      // لپ‌تاپ
+            slidesPerView: 3,
+            spaceBetween: 35,
+        },
+        1920: {      // دسکتاپ بزرگ
+            slidesPerView: 4,
+            spaceBetween: 40,
         }
     }
 });
+
+
+
+
+
+// Slider 3 (Teachers)
+new Swiper('.swiper-container3', {
+    loop: true,
+    centeredSlides: true,
+    centeredSlidesBounds: true,
+    spaceBetween: 32,
+    initialSlide: 0,
+    loopedSlides: 6,
+    breakpoints: {
+        1920: { slidesPerView: 3, loopedSlides: 3 },
+        1280: { slidesPerView: 2.5, loopedSlides: 2.5 },
+        768:  { slidesPerView: 2, loopedSlides: 2 },
+        480:  { slidesPerView: 1.2, loopedSlides: 1.2 },
+        100:  { slidesPerView: 1, loopedSlides: 1, }
+
+    }
+});
+
 
 
 
@@ -71,5 +78,6 @@ new Swiper('.swiper-container3', {
       slidesPerView: 1,
       spaceBetween: 20,
       pagination: { el: '.swiper-pagination', clickable: true },
+
     });
   });
